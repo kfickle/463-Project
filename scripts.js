049267b63a8b1d6c2548af8df42d168467b1ef82
Kahela Fickle <38086287+kfickle@users.noreply.github.com>
2023-06-06 19:06:07 -0700

    implementation of contact page

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
 // get the current year to insert into the footer
 let currentYear = new Date().getFullYear();
-document.getElementById('year').textContent = currentYear;
+document.getElementById("year").textContent = currentYear;
+
+// create an projects containing three elements
+$(document).ready(function () {
+  $(".read-more-btn").click(function () {
+    var $projectItem = $(this).closest(".project");
+    var $projectContent = $projectItem.find(".project-content");
+
+    $projectItem.toggleClass("active");
+    $projectContent.slideToggle();
+
+    var expanded = $projectItem.hasClass("active");
+    $projectItem.find(".read-more-btn").attr("aria-expanded", expanded);
+    $projectContent.attr("aria-hidden", !expanded);
+  });
+});
